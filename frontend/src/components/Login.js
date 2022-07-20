a524bc38d619b3ee18158abc34ab47ec9c5701f1
Marcin Chamera <mchamera237@gmail.com>
2022-07-20 15:50:29 +0200

    Dockerfile content change for backend & frontend + get rid of npm package not supported by react 18

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,8 +1,10 @@
 import { Container } from "@mui/system";
-import { Typography, Grid } from "@mui/material"
-import { GoogleLoginButton, GithubLoginButton, FacebookLoginButton } from "react-social-login-buttons";
+import { Typography, Grid, Button } from "@mui/material";
 import axios from "../axios/axios";
-import "../styles/General.css"
+import "../styles/General.css";
+import googleIcon from "../images/google.png";
+import githubIcon from "../images/github.png";
+import facebookIcon from "../images/facebook.png";
 
 const Login = () => {
     const redirectGoogle = () => {
@@ -17,11 +19,11 @@ const Login = () => {
         })
     }
 
-    const redirectFacebook = () => {
-        axios.get("/facebook/login").then((url) => {
-            window.open(url.data, "_self");
-        })
-    }
+    // const redirectFacebook = () => {
+    //     axios.get("/facebook/login").then((url) => {
+    //         window.open(url.data, "_self");
+    //     })
+    // }
 
     return (
         <Container className="container">
@@ -32,13 +34,22 @@ const Login = () => {
                     </Typography>
                 </Grid>
                 <Grid item sm={4} marginTop={5}>
-                    <GoogleLoginButton className="googleLogin" onClick={redirectGoogle} sx={{ maxWidth: 'md' }}/>
+                    <Button className="googleLogin" variant="outlined" onClick={redirectGoogle} sx={{ maxWidth: 'md' }}>
+                        <img src={googleIcon} alt="google icon" style={{ width: "30px", marginRight: 10}}/>
+                        Zaloguj przez Google
+                    </Button>
                 </Grid>
-                <Grid item sm={4}>
-                    <GithubLoginButton onClick={redirectGithub}/>
+                <Grid item sm={4} marginTop={2}>
+                    <Button className="githubLogin" variant="outlined" onClick={redirectGithub} sx={{ maxWidth: 'md' }}>
+                        <img src={githubIcon} alt="github icon" style={{ width: "30px", marginRight: 10}}/>
+                        Zaloguj przez Github
+                    </Button>
                 </Grid>
-                <Grid item sm={4}>
-                    <FacebookLoginButton onClick={() => alert("Jeszcze niedostępne")}/>
+                <Grid item sm={4} marginTop={2}>
+                    <Button className="facebookLogin" variant="outlined" onClick={() => alert("Jeszcze niedostępne")} sx={{ maxWidth: 'md' }}>
+                        <img src={facebookIcon} alt="facebook icon" style={{ width: "30px", marginRight: 10}}/>
+                        Zaloguj przez Facebook
+                    </Button>
                 </Grid>
             </Grid>
         </Container>
--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import { Card, CardContent, Typography, Container, Grid, CardMedia, Button, Alert, Snackbar } from "@mui/material";
 import { ProductsContext } from "../context/ProductsContext";
 import { ShoppingCartContext } from "../context/ShoppingCartContext";
