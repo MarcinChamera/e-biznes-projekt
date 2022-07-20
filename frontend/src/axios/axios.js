6f3daa4c8b1dc78b88fff52c7cbd76c1f2921bca
Marcin Chamera <mchamera237@gmail.com>
2022-07-20 20:35:09 +0200

    CORS changes

--- frontend/src/axios/axios.js
+++ frontend/src/axios/axios.js
@@ -6,7 +6,9 @@ const baseURL = "https://namelessshop-server.azurewebsites.net";
 export default axios.create({
     baseURL: baseURL,
     headers: {
+        'Access-Control-Allow-Origin': baseURL,
+        // 'Access-Control-Allow-Origin': baseURL + ":1323",
         "Content-Type": "application/json",
-        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
+        'Access-Control-Allow-Methods': "GET,PUT,POST,DELETE,PATCH,OPTIONS",
       }
 })
